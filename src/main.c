18287b2de76fd8d739cd9b0c5c9e754475bf17f7
Ion Buzdugan <ion.dbuzdugan@gmail.com>
2019-09-15 16:56:57 -0400

    Stepper motor spinning

--- src/main.c
+++ src/main.c
@@ -11,14 +11,53 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4xx_nucleo.h"
-			
+
+uint16_t p1 = GPIO_PIN_7;
+uint16_t p2 = GPIO_PIN_6;
+uint16_t p3 = GPIO_PIN_10;
+uint16_t p4 = GPIO_PIN_8;
+
+void run_motor(float d){
+  HAL_GPIO_WritePin(GPIOA,p1,1);
+  HAL_GPIO_WritePin(GPIOA,p2,0);
+  HAL_GPIO_WritePin(GPIOA,p3,0);
+  HAL_GPIO_WritePin(GPIOA,p4,0);
+
+  HAL_Delay(d);
+
+  HAL_GPIO_WritePin(GPIOA,p1,0);
+  HAL_GPIO_WritePin(GPIOA,p2,1);
+
+  HAL_Delay(d);
+
+  HAL_GPIO_WritePin(GPIOA,p2,0);
+  HAL_GPIO_WritePin(GPIOA,p3,1);
+
+  HAL_Delay(d);
+
+  HAL_GPIO_WritePin(GPIOA,p3,0);
+  HAL_GPIO_WritePin(GPIOA,p4,1);
+
+  HAL_Delay(d);
+}
+
+void init_pins(){
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  HAL_Init();
+  BSP_LED_Init(LED2);
+  __GPIOA_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_6 | GPIO_PIN_10 | GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
 
 int main(void)
 {
-  HAL_Init();
-  BSP_LED_Init(LED2);
-  while(1){
-    BSP_LED_Toggle(LED2);
-    HAL_Delay(1000);
+  init_pins();
+  while(10){
+    run_motor(10);
   }
 }
