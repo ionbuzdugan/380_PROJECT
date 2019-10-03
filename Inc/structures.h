8cff7972f6e9289a8feb1bfbc5e8352856d9bcc0
Ion Buzdugan <ion.dbuzdugan@gmail.com>
2019-10-02 21:21:07 -0400

    DMA Implemented and working. Next: test all motors to ensure proper pins

---
+++ Inc/structures.h
@@ -0,0 +1,19 @@
+#define LSB_MOTOR 0xA8
+#define LSB_CONNECT 0xB9
+#define CMD_M1 1
+#define CMD_M2 2
+#define CMD_M3 3
+#define CMD_M4 4
+#define CMD_M5 5
+#define CMD_M6 6
+#define MAX_CYCLES_PER_STEP 20
+#define MAX_MOTOR_CMD 100.0
+#define MOTOR_DELAY 1
+#define CYCLE_DELAY 0
+#define false 0
+#define true 1
+
+typedef struct{
+    int8_t payload[6];
+    uint8_t new;
+}MotorCmdFrame;
---
+++ Src/run.py
@@ -0,0 +1,48 @@
+import serial as serial
+import time 
+from ctypes import *
+import pdb
+
+LSB_CONNECT = b'\xB9'
+LSB_MOTOR = b'\xA8'
+
+s = serial.Serial(
+    port='/dev/ttyACM0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+def MotorCmd(m1=0,m2=0,m3=0,m4=0,m5=0,m6=0):
+    s.write(LSB_MOTOR)
+    s.write(c_int8(m1))
+    s.write(c_int8(m2))
+    s.write(c_int8(m3))
+    s.write(c_int8(m4))
+    s.write(c_int8(m5))
+    s.write(c_int8(m6))
+    s.write(c_uint8(1))
+
+def ConnectCmd():
+    s.write(LSB_CONNECT)
+    s.write(c_int8(0))
+    s.write(c_int8(0))
+    s.write(c_int8(0))
+    s.write(c_int8(0))
+    s.write(c_int8(0))
+    s.write(c_int8(0))
+    s.write(c_uint8(0))
+
+if __name__ == "__main__":
+    # Establish connection
+    print ("CONNECTING...\n")
+    r = "asfd"
+    while("ACK" not in r):
+        ConnectCmd()
+        r = str(s.read(3))
+    print ("CONNECTED\n")
+
+    pdb.set_trace()
+    while(1):
+        MotorCmd(50,0,0,0,0,0)
